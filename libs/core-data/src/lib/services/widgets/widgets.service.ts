5b3efcae21563b99c2b8206e5dcd6f6a90b383f6
Diego Acosta <diegoacosta211@gmail.com>
2023-05-28 12:31:19 -0300

    widget service code && json server

--- libs/core-data/src/lib/services/widgets/widgets.service.ts
+++ libs/core-data/src/lib/services/widgets/widgets.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Widget } from '@fem/api-interfaces';
+// import { environment } from '@env/environment';
 
+const API_URL = 'http://localhost:3000';
 @Injectable({
   providedIn: 'root'
 })
 export class WidgetsService {
+  model = 'widgets';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  all() {
+    return this.http.get<Widget[]>(this.getUrl());
+  }
+
+  find(id: string) {
+    return this.http.get<Widget>(this.getUrlWithId(id));
+  }
+
+  create(widget: Widget) {
+    return this.http.post(this.getUrl(), widget);
+  }
+
+  update(widget: Widget) {
+    return this.http.put(this.getUrlWithId(widget.id), widget);
+  }
+
+  delete(widget: Widget) {
+    return this.http.delete(this.getUrlWithId(widget.id));
+  }
+
+  private getUrl() {
+    return `${API_URL}${this.model}`;
+  }
+
+  private getUrlWithId(id) {
+    return `${this.getUrl()}/${id}`;
+  }
 }
